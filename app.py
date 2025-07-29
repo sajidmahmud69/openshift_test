48feff16892b33c1e6c619165545339226a00ca3
sajidmahmud69 <sajidmahmud136@yahoo.com>
2025-07-29 12:34:13 -0400

    second commit

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'Hello From NoWhere!'
+    return '<p>Hello From NoWhere!</p>'
+
+if __name__ == '__main__':
+    app.run(debug = True)
